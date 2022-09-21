012a3d81fe4242b2fe1d2722afb3a13c6d230d46
KuzmaNikita <113753628+KuzmaNikita@users.noreply.github.com>
2022-09-21 13:24:01 +0300

    Add files via upload

--- TASK2.py
+++ TASK2.py
@@ -1,7 +1,8 @@
-import math;
-r = float(input("Радіус "));
-h = float(input("Висота "));
-V = math.pi * (r*2)*h;
-S = 2*math.pi * r *(r+h);
-print(V);
-print(S);
+import math
+import sys
+r = float(sys.argv[1])
+h = float(sys.argv[2])
+V = math.pi * (r*2)*h
+S = 2*math.pi * r *(r+h)
+print(V)
+print(S)
