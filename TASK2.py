b79c5666128654a1dda9c9cc3e4da50a7b4fbd2f
KuzmaNikita <113753628+KuzmaNikita@users.noreply.github.com>
2022-09-23 12:55:23 +0300

    Add files via upload

--- TASK2.py
+++ TASK2.py
@@ -1,8 +1,8 @@
-import math
-import sys
-r = float(sys.argv[1])
-h = float(sys.argv[2])
-V = math.pi * (r*2)*h
-S = 2*math.pi * r *(r+h)
-print(V)
-print(S)
+a=float(input('Длина грани куба:'))/2
+x1=abs(float(input('Координата точки х:')))
+y1=abs(float(input('Координата точки y:')))
+z1=abs(float(input('Координата точки z:')))
+if x1<=a and y1<=a and z1<=a:
+    print('Принадлежит')
+else:
+    print('Не принадлежит')    
