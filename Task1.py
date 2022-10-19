8b1cacdab679e6099cd86a7eec7ecd5157c49cc8
KuzmaNikita <113753628+KuzmaNikita@users.noreply.github.com>
2022-10-19 12:36:39 +0300

    Add files via upload

--- Task1.py
+++ Task1.py
@@ -1,26 +1,22 @@
-from array import * 
-from math import pow
-import random
+from math import sqrt
 
-n = int(input("Введіть число n: ")) 
+#1. Три кортежі містять координати X, Y, Z точок у просторі. Програма 
+# повинна: 1) записати у список відстані від цих точок до центра координат; 2) 
+# вивести індекс точки, найближчої до початку координат; 3) вивести координати 
+# точки, найдільшої від центру координат. 
 
-arr = array('f', []) 
+tuples = [(-5, 8, 4), (-1, 2, -3), (3.4, 52, 9)]
+max_distant_dot = (0, ())
+list_of_distants = []
 
-for i in range(n): 
-    arr.append(float(pow(0.5, i)))
-     
-for i in range(n):
-    x = arr.append(random.randint(-1, 3)) 
+for i in tuples:
+    x, y, z = i
+    distant = round(sqrt(x ** 2 + y ** 2 + z ** 2), 4)
+    list_of_distants.append(distant)
 
-arr = array("f", sorted(arr.tolist(), reverse=True))
-print(arr)
+    if distant > max_distant_dot[0]:
+        max_distant_dot = (distant, i)
 
-
-arr.remove(arr[-1])
-arr.remove(arr[-1])
-
-
-arr.remove(arr[0])
-arr.remove(arr[0])
-
-print(arr)
+print(f"Відстані від точок до центру координат - {list_of_distants}")
+print(f"Індекс найближчої до центру точки - {list_of_distants.index(min(list_of_distants))}")
+print(f"Координати найдальшої від центру точки - {max_distant_dot[1]}")
