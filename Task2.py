c2ac8c72ed5eb97bebf76e20fa328a1caed3be61
KuzmaNikita <113753628+KuzmaNikita@users.noreply.github.com>
2022-11-02 12:46:47 +0200

    Add files via upload

--- Task2.py
+++ Task2.py
@@ -1,10 +1,32 @@
-import os
+import json
 
-#disc = os.getcwd()[:3]
+# 2. Текстовий файл містить дані про назви і довжини сторін трикутника у 
+# форматі JSON. Написати програму, яка: 1) перевірить існування трикутника; 2) 
+# виведе вид трикутника (прямокутний, рівнобедрений, рівносторонній). 
 
-# Проверит только текущую папку
-disc = os.getcwd()
+f = open("data.json", "r")
 
-for i in os.walk(disc):
-    if "input.txt" in i[-1]:
-        print(f"Файл input.txt найден по пути - {i[0]}")
+data = json.load(f)
+keys = list(data.keys())
+f.close()
+
+if data[keys[0]] < data[keys[1]] + data[keys[2]]:
+    print("Трикутник існує")
+    if data[keys[0]] == data[keys[1]] and data[keys[0]] == data[keys[2]]:
+        print("Рівносторонній") 
+    else:
+
+        if data[keys[0]] ** 2 == (data[keys[1]] ** 2 + data[keys[2]] ** 2) or data[keys[1]] ** 2 == (data[keys[0]] ** 2 + data[keys[2]] ** 2) or data[keys[2]] ** 2 == (data[keys[1]] ** 2 + data[keys[0]] ** 2):
+            
+            if data[keys[0]] == data[keys[1]] or data[keys[0]] == data[keys[2]] or data[keys[2]] == data[keys[1]]:
+                print("Прямокутний рівнобедренний")
+            else:
+                print("Прямокутний")
+
+
+        else:
+            if data[keys[0]] == data[keys[1]] or data[keys[0]] == data[keys[2]] or data[keys[2]] == data[keys[1]]:
+                print("Рівнобедренний")
+
+else:
+    print("Трикутник не існує")
