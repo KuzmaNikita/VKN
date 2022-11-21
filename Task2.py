581b9b68689cb8078a112d51581a1b716410c23a
KuzmaNikita <113753628+KuzmaNikita@users.noreply.github.com>
2022-11-21 14:09:40 +0200

    Add files via upload

--- Task2.py
+++ Task2.py
@@ -1,31 +1,28 @@
-from my_module2 import Complex
+import requests, random
+from bs4 import BeautifulSoup
 
-# 2. Розробити модуль, в якому описати клас COMPLEX (комплексне число) 
-# з властивостями: Real (дійсна частина), Imaginary (уявна частина). Написати для 
-# цього класу методи: конструктор класу, декструктор класу, SetReal (встановлює 
-# значення дійсної частини числа), SetImaginary (встановлює значення уявної 
-# частини числа), ShowComplex (виводить дійсну і уявну частину на екран). В 
-# основній програмі створит 2 екземпляри класу COMPLEX, виконати над цими 
-# об´єктами такі дії: додавання і віднімання.  
-
-# Створюємо 2 екземпляри класу
-f = Complex(5, "3")
-s = Complex(2, "6")
+# 2. Написати програму для завантаження з веб-сторінки 3-4 випадкових 
+# зображень, тематика яких пов´язана з розробкою програмного забезпечення. 
+# Сайт знайти самостійно. 
 
 
-if f.Imaginary + s.Imaginary < 0:
-    sumc = f"{f.Real + s.Real} - {abs(f.Imaginary + s.Imaginary)}i"
-else:
-    sumc = f"{f.Real + s.Real} + {f.Imaginary + s.Imaginary}i"
+def download_img(url, name):
+    print(url)
+    img = requests.get(url)
+
+    # Зберігаємо нашу картинку в поточну папку
+    with open(name, 'wb') as f: 
+        f.write(img.content)
 
 
-if f.Imaginary - s.Imaginary < 0:
-    razc = f"{f.Real - s.Real} - {abs(f.Imaginary - s.Imaginary)}i"
-else:
-    razc = f"{f.Real - s.Real} + {abs(f.Imaginary - s.Imaginary)}i"
+url = "https://unsplash.com/s/photos/programming-language"
+req = requests.get(url).text
+bs = BeautifulSoup(req, "html.parser")
 
 
-print(sumc)
-print(razc)
+imgs = bs.find("div", {"class": "mItv1"}).findAll("img")
 
+
+for i in range(4):
+    download_img(random.choice(imgs)["src"], f"image{i}.jpeg")
 
