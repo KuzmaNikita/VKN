8a838f38066052ffd747e4ca3ab8009ed381f7f7
KuzmaNikita <113753628+KuzmaNikita@users.noreply.github.com>
2022-11-21 12:57:55 +0200

    Add files via upload

--- Task1.py
+++ Task1.py
@@ -1,10 +1,42 @@
-from my_module1 import *
+from random import shuffle
 
-# 1. Написати програму, яка виконуватиме такі операції: 1) виведення на 
-# екран дільників, спільних для двох натуральних чисел; 2) виведення на екран 
-# перших N чисел, кратних заданому числу. Ці операції реалізувати у вигляді 
-# функцій. 
+# 1. Створити клас BOOKSHELF (книжкова полиця) з такими 
+# властивостями: назва, список назв книжок; методами: конструктор класу, 
+# додання книги на полицю, видалення книги з полиці, перемішування книг на 
+# полиці. Написати програму, яка імітуватиме книжкову поличку. 
 
 
-find_delyteli(10, 10)
-crat(5, 2)
+class BOOKSHELF():
+
+    
+    def __init__(self, name, books_list):
+        self.name = str(name)
+        self.book_list = books_list
+
+    
+    
+    def add_book(self, book_name):
+        self.book_list.append(book_name)
+    
+    
+    def delete_book(self, book_name):
+        self.book_list.remove(book_name)
+
+    
+    def shuffle_books(self):
+        shuffle(self.book_list)
+
+
+shelf = BOOKSHELF("Домашня", ["Воно", "1984", "Острів скарбів", "Білий Клик", "Ми", "Війна та мир"])
+
+
+print(shelf.name)
+print(shelf.book_list)
+
+
+shelf.add_book("Гаррі Поттер")
+shelf.add_book("Місто")
+shelf.delete_book("Ми")
+print(shelf.book_list)
+shelf.shuffle_books()
+print(shelf.book_list)
